f8ccc3cd903ce86d7ef230c983a2dde7a6ca5f6e
Rustam Sadreev <rustam17.sb@gmail.com>
2024-11-17 21:03:50 +0300

    feat: add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FilmPage from '../../pages/film-page/film-page';
+import MyListPage from '../../pages/my-list-page/my-list-page';
+import ReviewPage from '../../pages/review-page/review-page';
+import PlayerPage from '../../pages/player-page/player-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+
 import { PromoFilm } from '../../types/promo-film';
 
 type AppProps = {
@@ -7,7 +17,38 @@ type AppProps = {
 
 function App({ promoFilm }: AppProps): JSX.Element {
   return (
-    <MainPage promoFilm={promoFilm} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage promoFilm={promoFilm} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<FilmPage />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyListPage />}
+        />
+        <Route
+          path={AppRoute.Review}
+          element={<ReviewPage />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,1 +1,10 @@
 export const FILMS_PER_LOAD = 8;
+
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  MyList = '/mylist',
+  Film = '/films/:id',
+  Review = '/films/:id/review',
+  Player = '/player/:id',
+}
