f900ec7ba2429fe7e0b0d7caa6dff259f26edeb7
Rustam Sadreev <rustam17.sb@gmail.com>
2024-11-17 20:50:58 +0300

    feat: add 404 page component

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,37 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <h1 className="page-title user-page__title">404 Not Found</h1>
+      </header>
+
+      <div className="sign-in user-page__content">
+        <a className="sign-in__btn" href="main.html" style={{ textDecoration: 'none' }}>Go to Homepage</a>
+      </div>
+
+      <footer className="page-footer">
+        <div className="logo">
+          <a href="main.html" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default NotFoundPage;
