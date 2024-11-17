fc7671b839c2f25fdecd3b7584211c614f306303
Rustam Sadreev <rustam17.sb@gmail.com>
2024-11-17 21:18:35 +0300

    feat: make '/mylist' and '/review' routes private

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 import MainPage from '../../pages/main-page/main-page';
 import LoginPage from '../../pages/login-page/login-page';
@@ -8,6 +8,7 @@ import MyListPage from '../../pages/my-list-page/my-list-page';
 import ReviewPage from '../../pages/review-page/review-page';
 import PlayerPage from '../../pages/player-page/player-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
 
 import { PromoFilm } from '../../types/promo-film';
 
@@ -33,11 +34,19 @@ function App({ promoFilm }: AppProps): JSX.Element {
         />
         <Route
           path={AppRoute.MyList}
-          element={<MyListPage />}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <MyListPage />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Review}
-          element={<ReviewPage />}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <ReviewPage />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Player}
