403a7ea4903f6ca743a7a36b32d74a38535213cd
Rustam Sadreev <rustam17.sb@gmail.com>
2024-11-17 21:12:50 +0300

    feat: add a private route component

--- src/const.ts
+++ src/const.ts
@@ -8,3 +8,9 @@ export enum AppRoute {
   Review = '/films/:id/review',
   Player = '/player/:id',
 }
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
