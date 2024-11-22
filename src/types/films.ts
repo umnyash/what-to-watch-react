dde5d082e7848096c9ea990adaf4e22bab57fa08
Rustam Sadreev <rustam17.sb@gmail.com>
2024-11-23 00:36:34 +0300

    feat: add mock films data

---
+++ src/types/films.ts
@@ -0,0 +1,30 @@
+type BaseFilm = {
+  id: string;
+  name: string;
+  genre: string;
+}
+
+export type CardFilm = BaseFilm & {
+  previewImage: string;
+  previewVideoLink: string;
+}
+
+export type PromoFilm = BaseFilm & {
+  posterImage: string;
+  backgroundImage: string;
+  videoLink: string;
+  released: number;
+  isFavorite: boolean;
+};
+
+export type PageFilm = PromoFilm & {
+  backgroundColor: string;
+  rating: number;
+  description: string;
+  scoresCount: number;
+  director: string;
+  starring: string[];
+  runTime: number;
+}
+
+export type Films = CardFilm[];
