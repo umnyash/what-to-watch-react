07d6e4c080536e2e25fa56435b2e102a88e6740e
Rustam Sadreev <rustam17.sb@gmail.com>
2025-03-04 08:07:12 +0300

    feat: disable 404 and 401 error notifications

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,8 +12,6 @@ type ErrorResponseData = {
 
 const StatusCodeMapping: Record<number, boolean> = {
   [StatusCodes.BAD_REQUEST]: true,
-  [StatusCodes.UNAUTHORIZED]: true,
-  [StatusCodes.NOT_FOUND]: true,
   [StatusCodes.CONFLICT]: true,
 };
 
