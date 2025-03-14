0a69985ab5fc70e148b1729a851fabc6be03b7ce
Rustam Sadreev <rustam17.sb@gmail.com>
2025-03-14 21:29:34 +0300

    feat: implement custom login form validation with error messages

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -1,18 +1,54 @@
-import { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import { useState, useRef, useEffect, ChangeEvent, FormEvent } from 'react';
 import useAppDispatch from '../../hooks/use-app-dispatch';
 import useAppSelector from '../../hooks/use-app-selector';
 import { selectors } from '../../store/selectors';
 import { clearLoginErrorData } from '../../store/actions';
 import { loginUser } from '../../store/async-actions';
 import Button, { ButtonType, ButtonSize } from '../button';
+import { getValidationErrorMessage } from '../../validation';
+import clsx from 'clsx';
+
+const EMAIL_FIELD_ID = 'user-email';
+const PASSWORD_FIELD_ID = 'user-password';
 
 function LoginForm(): JSX.Element {
+  const emailInputElementRef = useRef<HTMLInputElement | null>(null);
+  const passwordInputElementRef = useRef<HTMLInputElement | null>(null);
+  const [activeFieldId, setActiveFieldId] = useState('');
+
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
 
   const isLoggingIn = useAppSelector(selectors.isLoggingIn);
+  const emailErrorMessage = getValidationErrorMessage('email', formData.email);
+  const passwordErrorMessage = getValidationErrorMessage('password', formData.password);
+  let errorMessage: string | null = null;
+
+  switch (activeFieldId) {
+    case EMAIL_FIELD_ID:
+      errorMessage = emailErrorMessage;
+      break;
+    case PASSWORD_FIELD_ID:
+      errorMessage = passwordErrorMessage;
+      break;
+  }
+
+  const emailFieldClassName = clsx(
+    'sign-in__field',
+    activeFieldId === EMAIL_FIELD_ID &&
+    emailErrorMessage &&
+    'sign-in__field--error'
+  );
+
+  const passwordFieldClassName = clsx(
+    'sign-in__field',
+    activeFieldId === PASSWORD_FIELD_ID &&
+    passwordErrorMessage &&
+    'sign-in__field--error'
+  );
+
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -25,18 +61,40 @@ function LoginForm(): JSX.Element {
     dispatch(clearLoginErrorData());
   };
 
+  const handleFieldBlur = () => {
+    setActiveFieldId('');
+  };
+
   const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    dispatch(loginUser(formData));
+
+    switch (true) {
+      case Boolean(emailErrorMessage):
+        emailInputElementRef.current?.focus();
+        setActiveFieldId(EMAIL_FIELD_ID);
+        break;
+      case Boolean(passwordErrorMessage):
+        passwordInputElementRef.current?.focus();
+        setActiveFieldId(PASSWORD_FIELD_ID);
+        break;
+      default:
+        dispatch(loginUser(formData));
+    }
   };
 
   return (
     <form className="sign-in__form" method="post" action="#" noValidate onSubmit={handleFormSubmit}>
+      {errorMessage && (
+        <div className="sign-in__message">
+          <p>{errorMessage}</p>
+        </div>
+      )}
       <div className="sign-in__fields">
-        <div className="sign-in__field">
+        <div className={emailFieldClassName}>
           <input
+            ref={emailInputElementRef}
             className="sign-in__input"
-            id="user-email"
+            id={EMAIL_FIELD_ID}
             type="email"
             name="email"
             value={formData.email}
@@ -44,13 +102,15 @@ function LoginForm(): JSX.Element {
             required
             disabled={isLoggingIn}
             onChange={handleFieldChange}
+            onBlur={handleFieldBlur}
           />
           <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
         </div>
-        <div className="sign-in__field">
+        <div className={passwordFieldClassName}>
           <input
+            ref={passwordInputElementRef}
             className="sign-in__input"
-            id="user-password"
+            id={PASSWORD_FIELD_ID}
             type="password"
             name="password"
             value={formData.password}
@@ -58,6 +118,7 @@ function LoginForm(): JSX.Element {
             required
             disabled={isLoggingIn}
             onChange={handleFieldChange}
+            onBlur={handleFieldBlur}
             pattern="(?=.*[a-zA-Z])(?=.*\d).*"
             title="Пароль должен состоять минимум из одной буквы и цифры."
           />
---
+++ src/validation.ts
@@ -0,0 +1,58 @@
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const PASSWORD_REGEXP = /(?=.*[a-zA-Z])(?=.*\d).*/;
+const PASSWORD_MIN_LENGTH = 2;
+
+const validationErrorMessages = {
+  email: {
+    required: 'Email cannot be empty',
+    pattern: 'Please enter a valid email address.',
+  },
+  password: {
+    required: 'Password cannot be empty.',
+    pattern: 'Password must contain letters and numbers.',
+    minLength: `Password must be at least ${PASSWORD_MIN_LENGTH} characters long.`,
+  },
+} as const;
+
+const ValidationRules = {
+  email: {
+    required: {
+      message: validationErrorMessages.email.required,
+    },
+    pattern: {
+      value: EMAIL_REGEXP,
+      message: validationErrorMessages.email.pattern,
+    },
+  },
+  password: {
+    required: {
+      message: validationErrorMessages.password.required,
+    },
+    pattern: {
+      value: PASSWORD_REGEXP,
+      message: validationErrorMessages.password.pattern,
+    },
+    minLength: {
+      value: PASSWORD_MIN_LENGTH,
+      message: validationErrorMessages.password.minLength,
+    }
+  }
+} as const;
+
+export const getValidationErrorMessage = (type: keyof typeof ValidationRules, value: string) => {
+  const rules = ValidationRules[type];
+
+  if (!value.length) {
+    return rules.required.message;
+  }
+
+  if (!rules.pattern.value.test(value)) {
+    return rules.pattern.message;
+  }
+
+  if ('minLength' in rules && value.length < rules.minLength.value) {
+    return rules.minLength.message;
+  }
+
+  return null;
+};
