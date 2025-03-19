eab1fd3b3aaa31222a1fb98b51491899efc0433b
Rustam Sadreev <rustam17.sb@gmail.com>
2025-03-19 10:15:31 +0300

    refactor: add catalog component

---
+++ src/components/catalog/catalog.tsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react';
+import useAppDispatch from '../../hooks/use-app-dispatch';
+import useAppSelector from '../../hooks/use-app-selector';
+import { catalogSelectors } from '../../store/catalog/catalog.selectors';
+import { fetchFilms } from '../../store/async-actions';
+import Spinner from '../spinner';
+import GenresList from '../genres-list';
+import FilmsList from '../films-list';
+import Button from '../button';
+import ErrorMessage from '../error-message';
+import { Films, FilmsByGenre } from '../../types/films';
+import { ALL_GENRES, FILMS_PER_LOAD } from '../../const';
+
+function groupFilmsByGenre(films: Films) {
+  return films.reduce((genreMap: FilmsByGenre, film) => {
+    const genre = film.genre;
+
+    if (!genreMap[genre]) {
+      genreMap[genre] = [];
+    }
+
+    genreMap[genre].push(film);
+    return genreMap;
+  }, {});
+}
+
+function Catalog(): JSX.Element {
+  const [displayedFilmsMaxCount, setDisplayedFilmsMaxCount] = useState(FILMS_PER_LOAD);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFilms());
+  }, [dispatch]);
+
+  const activeGenre = useAppSelector(catalogSelectors.genre);
+  const films = useAppSelector(catalogSelectors.films);
+  const isFilmsLoading = useAppSelector(catalogSelectors.isFilmsLoading);
+  const isFilmsLoaded = useAppSelector(catalogSelectors.isFilmsLoaded);
+  const isFilmsLoadFailed = useAppSelector(catalogSelectors.isFilmsLoadFailed);
+  const filmsByGenre = groupFilmsByGenre(films);
+  const filmsByActiveGenre = (activeGenre === ALL_GENRES) ? films : filmsByGenre[activeGenre];
+
+  const handleShowMoreButtonClick = () => {
+    setDisplayedFilmsMaxCount((count) => count + FILMS_PER_LOAD);
+  };
+
+  const handleGenreClick = () => {
+    setDisplayedFilmsMaxCount(FILMS_PER_LOAD);
+  };
+
+  return (
+    <section className="catalog">
+      <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+      {isFilmsLoading && <Spinner />}
+
+      {isFilmsLoadFailed && (
+        <ErrorMessage
+          text="We couldn&apos;t load the films. Please try again later."
+          onButtonClick={() => {
+            dispatch(fetchFilms());
+          }}
+        />
+      )}
+
+      {isFilmsLoaded && (
+        <>
+          <GenresList activeGenre={activeGenre} filmsByGenre={filmsByGenre} onGenreClick={handleGenreClick} />
+          <FilmsList films={filmsByActiveGenre.slice(0, displayedFilmsMaxCount)} />
+
+          {filmsByActiveGenre.length > displayedFilmsMaxCount && (
+            <div className="catalog__more">
+              <Button onClick={handleShowMoreButtonClick}>Show more</Button>
+            </div>
+          )}
+        </>
+      )}
+    </section>
+  );
+}
+
+export default Catalog;
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,60 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import useAppDispatch from '../../hooks/use-app-dispatch';
 import useAppSelector from '../../hooks/use-app-selector';
-import { catalogSelectors } from '../../store/catalog/catalog.selectors';
 import { promoFilmSelectors } from '../../store/promo-film/promo-film.selectors';
-import { fetchPromoFilm, fetchFilms } from '../../store/async-actions';
+import { fetchPromoFilm } from '../../store/async-actions';
 import SiteHeader from '../../components/site-header';
 import FilmHeader from '../../components/film-header';
-import Spinner from '../../components/spinner';
-import GenresList from '../../components/genres-list';
-import FilmsList from '../../components/films-list';
-import Button from '../../components/button';
-import ErrorMessage from '../../components/error-message';
-import { Films, FilmsByGenre } from '../../types/films';
-import { ALL_GENRES, FILMS_PER_LOAD } from '../../const';
+import Catalog from '../../components/catalog';
 import { Helmet } from 'react-helmet-async';
 import SiteFooter from '../../components/site-footer';
 
-function groupFilmsByGenre(films: Films) {
-  return films.reduce((genreMap: FilmsByGenre, film) => {
-    const genre = film.genre;
-
-    if (!genreMap[genre]) {
-      genreMap[genre] = [];
-    }
-
-    genreMap[genre].push(film);
-    return genreMap;
-  }, {});
-}
-
 function MainPage(): JSX.Element {
-  const [displayedFilmsMaxCount, setDisplayedFilmsMaxCount] = useState(FILMS_PER_LOAD);
-
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchPromoFilm());
-    dispatch(fetchFilms());
   }, [dispatch]);
 
   const promoFilm = useAppSelector(promoFilmSelectors.film);
-  const activeGenre = useAppSelector(catalogSelectors.genre);
-  const films = useAppSelector(catalogSelectors.films);
-  const isFilmsLoading = useAppSelector(catalogSelectors.isFilmsLoading);
-  const isFilmsLoaded = useAppSelector(catalogSelectors.isFilmsLoaded);
-  const isFilmsLoadFailed = useAppSelector(catalogSelectors.isFilmsLoadFailed);
-  const filmsByGenre = groupFilmsByGenre(films);
-  const filmsByActiveGenre = (activeGenre === ALL_GENRES) ? films : filmsByGenre[activeGenre];
-
-  const handleShowMoreButtonClick = () => {
-    setDisplayedFilmsMaxCount((count) => count + FILMS_PER_LOAD);
-  };
-
-  const handleGenreClick = () => {
-    setDisplayedFilmsMaxCount(FILMS_PER_LOAD);
-  };
 
   return (
     <>
@@ -85,34 +47,7 @@ function MainPage(): JSX.Element {
       </section>
 
       <div className="page-content">
-        <section className="catalog">
-          <h2 className="catalog__title visually-hidden">Catalog</h2>
-
-          {isFilmsLoading && <Spinner />}
-
-          {isFilmsLoadFailed && (
-            <ErrorMessage
-              text="We couldn&apos;t load the films. Please try again later."
-              onButtonClick={() => {
-                dispatch(fetchFilms());
-              }}
-            />
-          )}
-
-          {isFilmsLoaded && (
-            <>
-              <GenresList activeGenre={activeGenre} filmsByGenre={filmsByGenre} onGenreClick={handleGenreClick} />
-              <FilmsList films={filmsByActiveGenre.slice(0, displayedFilmsMaxCount)} />
-
-              {filmsByActiveGenre.length > displayedFilmsMaxCount && (
-                <div className="catalog__more">
-                  <Button onClick={handleShowMoreButtonClick}>Show more</Button>
-                </div>
-              )}
-            </>
-          )}
-        </section>
-
+        <Catalog />
         <SiteFooter />
       </div>
     </>
