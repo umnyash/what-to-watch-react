e0c262f4d29dda84593abd2fcbb699145bf8e1e4
Rustam Sadreev <rustam17.sb@gmail.com>
2025-03-20 16:51:28 +0300

    feat: add error page component

---
+++ src/pages/error-page/error-page.tsx
@@ -0,0 +1,21 @@
+import SiteHeader from '../../components/site-header';
+import ErrorMessage from '../../components/error-message';
+
+type ErrorPageProps = {
+  text: string;
+  onRetryButtonClick: () => void;
+}
+
+function ErrorPage({ text, onRetryButtonClick }: ErrorPageProps): JSX.Element {
+  return (
+    <div className="user-page">
+      <SiteHeader className="user-page__head" withUserNavigation />
+      <ErrorMessage
+        text={text}
+        onButtonClick={onRetryButtonClick}
+      />
+    </div>
+  );
+}
+
+export default ErrorPage;
