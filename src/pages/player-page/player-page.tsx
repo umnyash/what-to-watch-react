de2758d9069fe2b2f33e969fb11e7d2bdac7f729
Rustam Sadreev <rustam17.sb@gmail.com>
2025-03-22 22:24:09 +0300

    feat: implement data retrieval for the video player

--- src/pages/player-page/player-page.tsx
+++ src/pages/player-page/player-page.tsx
@@ -1,19 +1,65 @@
+import { useEffect } from 'react';
 import { Link, useLocation, Location, useParams } from 'react-router-dom';
 import { LocationState } from '../../types/location';
 import { AppRoute, ROUTE_PARAM_ID } from '../../const';
 import { Helmet } from 'react-helmet-async';
+import useAppSelector from '../../hooks/use-app-selector';
+import { filmSelectors } from '../../store/film/film.selectors';
+import { promoFilmSelectors } from '../../store/promo-film/promo-film.selectors';
+import useAppDispatch from '../../hooks/use-app-dispatch';
+import { fetchFilm } from '../../store/async-actions';
+
+import LoadingPage from '../loading-page';
+import NotFoundPage from '../not-found-page';
+import ErrorPage from '../error-page';
 
 function PlayerPage(): JSX.Element {
   const location = useLocation() as Location<LocationState>;
   const filmId = useParams().id as string;
   const filmPageLink = AppRoute.Film.replace(ROUTE_PARAM_ID, filmId);
 
+  const film = useAppSelector(filmSelectors.film);
+  const promoFilm = useAppSelector(promoFilmSelectors.film);
+  const targetFilm = [film, promoFilm].find((item) => item?.id === filmId) ?? null;
+  const isFilmLoading = useAppSelector(filmSelectors.isLoading);
+  const isFilmLoadFailed = useAppSelector(filmSelectors.isLoadFailed);
+  const isNotFound = useAppSelector(filmSelectors.isNotFound);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (targetFilm) {
+      return;
+    }
+
+    dispatch(fetchFilm(filmId));
+  }, [filmId, targetFilm, dispatch]);
+
+  if (isFilmLoading) {
+    return <LoadingPage />;
+  }
+
+  if (isFilmLoadFailed || !targetFilm) {
+    return isNotFound
+      ? <NotFoundPage />
+      : (
+        <ErrorPage
+          text="We couldn&apos;t load the film. Please try again later."
+          onRetryButtonClick={() => {
+            dispatch(fetchFilm(filmId));
+          }}
+        />
+      );
+  }
+
+  const { name, backgroundImage, videoLink } = targetFilm;
+
   return (
     <div className="player">
       <Helmet>
         <title>WTW: Player</title>
       </Helmet>
-      <video src="#" className="player__video" poster="img/player-poster.jpg"></video>
+      <video src={videoLink} className="player__video" poster={backgroundImage} />
 
       <Link
         className="player__exit"
@@ -39,7 +85,7 @@ function PlayerPage(): JSX.Element {
             </svg>
             <span>Play</span>
           </button>
-          <div className="player__name">Transpotting</div>
+          <div className="player__name">{name}</div>
 
           <button type="button" className="player__full-screen">
             <svg viewBox="0 0 27 27" width="27" height="27">
