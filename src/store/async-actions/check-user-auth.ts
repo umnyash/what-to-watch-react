4f85d1f010b8a25354797c3d77f2e5d462382f30
Rustam Sadreev <rustam17.sb@gmail.com>
2025-06-27 14:39:23 +0300

    refactor: use omit function for extract user data on successful auth check

--- src/store/async-actions/check-user-auth.ts
+++ src/store/async-actions/check-user-auth.ts
@@ -1,11 +1,13 @@
 import createAppAsyncThunk from '../../hooks/create-app-async-thunk';
 import { APIRoute, SliceName } from '../../const';
 import { AuthUser, User } from '../../types/user';
+import { omit } from '../../util';
 
 export const checkUserAuth = createAppAsyncThunk<User, undefined>(
   `${SliceName.User}/checkAuth`,
   async (_arg, { extra: { api } }) => {
-    const { data: { name, email, avatarUrl } } = await api.get<AuthUser>(APIRoute.Login);
-    return { name, email, avatarUrl };
+    const { data } = await api.get<AuthUser>(APIRoute.Login);
+    const user = omit(data, 'token');
+    return user;
   },
 );
