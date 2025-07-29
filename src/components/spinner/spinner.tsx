d3ec13d8dce6dd8ab9d8351a02b80bd295d57938
Rustam Sadreev <rustam17.sb@gmail.com>
2025-07-29 21:29:24 +0300

    feat: add hidden loading text for spinner accessibility

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -3,6 +3,7 @@ import style from './spinner.module.css';
 function Spinner(): JSX.Element {
   return (
     <div className={style.spinner}>
+      <p className="visually-hidden">Loading...</p>
       <svg width="72" height="72" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#d9cd8d">
         <circle cx="4" cy="12" r="3" />
         <circle cx="4" cy="12" r="3" />
