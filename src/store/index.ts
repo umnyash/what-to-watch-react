01caca35abc665f1e6e06d167af589f379a8758d
Rustam Sadreev <rustam17.sb@gmail.com>
2025-09-05 22:35:28 +0300

    refactor: add store factory with actions spy for tests

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,57 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Middleware, AnyAction } from '@reduxjs/toolkit';
 import { rootReducer } from './root-reducer';
 import { createAPI, isApiError } from '../services/api';
 import { fetchFavoritesOnAuth } from './middlewares/fetch-favorites-on-auth';
 
-const api = createAPI();
+interface WithActionsSpy {
+  getActions(): Array<AnyAction>;
+}
 
-export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      thunk: {
-        extraArgument: { api, isApiError }
-      },
-    }).concat(fetchFavoritesOnAuth.middleware),
-});
+type AppStore = ReturnType<typeof setupStore>;
+export type TestStore = AppStore & WithActionsSpy;
+
+type SetupStoreOptions = {
+  api?: ReturnType<typeof createAPI>;
+  preloadedState?: Partial<ReturnType<typeof rootReducer>>;
+  withActionsSpy?: boolean;
+};
+
+const createActionsSpy = () => {
+  const actions: AnyAction[] = [];
+
+  const middleware: Middleware = () => (next) => (action: AnyAction): AnyAction => {
+    actions.push(action);
+    return next(action);
+  };
+
+  const get = () => [...actions];
+
+  return { middleware, get };
+};
+
+export const setupStore = ({
+  api = createAPI(),
+  preloadedState,
+  withActionsSpy,
+}: SetupStoreOptions = {}) => {
+  const actionsSpy = withActionsSpy && createActionsSpy();
+
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        thunk: {
+          extraArgument: { api, isApiError },
+        },
+      })
+        .concat(fetchFavoritesOnAuth.middleware)
+        .concat(actionsSpy ? [actionsSpy.middleware] : []),
+  });
+
+  return actionsSpy
+    ? Object.assign(store, { getActions: actionsSpy.get })
+    : store;
+};
+
+export const store = setupStore();
